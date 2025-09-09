d3ec85b759dbdf22b703255cafa8f2fd09419294
Sumeet Borole <sumeet21borole@gmail.com>
2025-09-09 23:24:31 +0530

    node question read file

---
+++ node question/read-file.js
@@ -0,0 +1,13 @@
+//import { readFile } from "fs/promises";
+const readFile = require("fs").promises.readFile;
+
+const read = async () => {
+  try {
+    const data = await readFile("example.txt", "utf-8");
+    console.log(data);
+  } catch (err) {
+    console.log("error", err);
+  }
+};
+read();
+l;
