b95ef085c443dad460494f32f1f38141dd0bc9b3
Sumeet Borole <sumeet21borole@gmail.com>
2025-10-13 23:30:13 +0530

    typ2

--- typescript/script.ts
+++ typescript/script.ts
@@ -1,5 +1,27 @@
-let name : string = "TypeScript";
-//name = 21;
-//console.log(`Hello, ${name.toUpperCase()}!`);
+
+// Inteference = TypeScript can infer the type of a variable based on its value
+let name = "TypeScript";
+// //name = 21;    // Error: Type 'number' is not assignable to type 'string'
+// //console.log(`Hello, ${name.toUpperCase()}!`);
 console.log(`Hello, ${name}!`);
+
+
+
+// Explicitly setting the type of a variable
+let userID : String | number;
+userID = "12345";  // OK
+userID = 12345;    // OK
+function add(a: number, b: number|string) {
+    if (typeof b === 'number') {   // TypeScript now knows b is number here
+        return a + b;
+    } else {    // TypeScript now knows b is string here  
+        return a + b;
+    }
+}
+console.log(add(5, 10));        // Outputs: 15
+console.log(add(5, "10"));      // Outputs: 510
+
+
+
+
 
