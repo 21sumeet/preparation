f7549d3b87ad09a832b7d05514b2e27568cb366a
Sumeet Borole <sumeet21borole@gmail.com>
2025-10-15 23:05:43 +0530

    res

---
+++ typescript/rest.ts
@@ -0,0 +1,6 @@
+function add(a: number, b: number, c : number): number {
+    return a + b + c;
+}
+console.log(add(5, 10, 15));        // Outputs: 30
+console.log(add(5,4,3,2,1));      // Error: Expected 3 arguments, but got 5.
+
