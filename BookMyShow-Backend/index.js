58d9de278de9a191f7b7c7fe53f3b29285a44a23
Sumeet Borole <sumeet21borole@gmail.com>
2025-10-18 19:27:13 +0530

    rate limiter

--- BookMyShow-Backend/index.js
+++ BookMyShow-Backend/index.js
@@ -4,9 +4,19 @@ const express = require("express");
 const app = express();
 cont = dotenv = require("dotenv");
 dotenv.config();
+const rateLimit = require("express-rate-limit");
 const port = process.env.PORT || 3000;
 const mongoose = require("mongoose");
 //const movie = require("./models/movie.model");
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+  message: "Too many requests from this IP, please try again after 15 minutes",
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+app.use(limiter);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
