94d2da1ce5e006ffe4d4b0c00e379da7fb7a8671
Sumeet Borole <sumeet21borole@gmail.com>
2025-10-21 23:57:53 +0530

    roadsidercoder otp system

---
+++ roadsidecoder frontend/otp system/src/components/phone-login.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+
+const PhoneOtpForm = () => {
+  const [phone, setphone] = useState();
+  const [showotpinput, setshowotpinput] = useState(false);
+
+  const handlePhoneNumber = (e) => {
+    setphone(e.target.value);
+    console.log("Phone number submitted:", phone);
+  };
+
+  const handlePhoneSubmit = (e) => {
+    e.preventDefault();
+
+    // Validate phone number (basic validation)
+    const regex = /[^0-9]/g;
+    if (phone.length < 10 || regex.test(phone)) {
+      alert("Invalid Phone Number");
+      return;
+    }
+    setshowotpinput(true);
+  };
+
+  return (
+    <div>
+      {!showotpinput ? (
+        <form onSubmit={handlePhoneSubmit}>
+          <input
+            type="text"
+            name="phone"
+            value={phone}
+            onChange={handlePhoneNumber}
+            placeholder="Enter phone number"
+            required
+          />
+          <button type="submit">Send OTP</button>
+        </form>
+      ) : (
+        <div>
+          <p>Enter OTP sent to {phone}</p>
+          {/* <OtpInput length={4} onOtpSubmit={onOtpSubmit} /> */}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default PhoneOtpForm;
