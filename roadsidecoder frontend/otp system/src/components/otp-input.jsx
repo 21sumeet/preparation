a67db2b593135f562bd112031c5be2175632f8d0
Sumeet Borole <sumeet21borole@gmail.com>
2025-10-22 23:24:47 +0530

    complete

--- roadsidecoder frontend/otp system/src/components/otp-input.jsx
+++ roadsidecoder frontend/otp system/src/components/otp-input.jsx
@@ -1,11 +1,48 @@
-import React, { useState, useSyncExternalStore } from "react";
+import React, {
+  useState,
+  useSyncExternalStore,
+  useEffect,
+  useRef,
+} from "react";
 
 const OtpInput = ({ length = 4, onOtpSubmit = () => {} }) => {
   const [otp, setOtp] = useState(new Array(length).fill(""));
+  const inputRefs = useRef([]);
 
-  const handleChange = (e, index) => {};
-  const handleKeyDown = (e, index) => {};
-  const handleClick = (index) => {};
+  useEffect(() => {
+    inputRefs.current[0].focus();
+  }, []);
+
+  const handleChange = (index, e) => {
+    const value = e.target.value;
+    const newOtp = [...otp];
+    newOtp[index] = value.slice(-1);
+    setOtp(newOtp);
+
+    // Check if the OTP is complete
+    const combinedOtp = newOtp.join("");
+    if (combinedOtp.length === length) {
+      onOtpSubmit(combinedOtp);
+    }
+
+    // Focus on the next input
+    if (value && index < length - 1) {
+      inputRefs.current[index + 1].focus();
+    }
+  };
+  const handleKeyDown = (index, e) => {
+    if (e.key === "Backspace" && !otp[index] && index > 0) {
+      inputRefs.current[index - 1].focus();
+    }
+  };
+  const handleClick = (index) => {
+    inputRefs.current[index].setSelectionRange(1, 1);
+
+    // optional
+    if (index > 0 && !otp[index - 1]) {
+      inputRefs.current[otp.indexOf("")].focus();
+    }
+  };
 
   return (
     <div>
@@ -14,7 +51,7 @@ const OtpInput = ({ length = 4, onOtpSubmit = () => {} }) => {
           <input
             key={index}
             type="text"
-            //ref={(input) => (inputRefs.current[index] = input)}
+            ref={(input) => (inputRefs.current[index] = input)}
             value={value}
             onChange={(e) => handleChange(index, e)}
             onClick={() => handleClick(index)}
