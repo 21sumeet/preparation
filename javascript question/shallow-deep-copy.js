12ae2ee8729e62ce96dae758676af5f6108330da
Sumeet Borole <sumeet21borole@gmail.com>
2025-11-03 23:47:31 +0530

    deep copy

--- javascript question/shallow-deep-copy.js
+++ javascript question/shallow-deep-copy.js
@@ -14,3 +14,11 @@ console.log("shallow copy person name:", person.name); // Output: Bob
 console.log("shallow copy person name:", person.name); // Output: Alice
 shallowCopy.address.city = "Los Angeles";
 console.log("shallow copy person address city:", person.address.city); // Output: Los Angeles  :bcoz { },[] ,() are reference types so when we change in shallow copy it will reflect in original object
+
+// Deep Copy
+const deepCopy = JSON.parse(JSON.stringify(person));
+deepCopy.name = "Charlie";
+console.log("deep copy person name:", deepCopy.name); // Output: Charlie
+console.log("deep copy person name:", person.name); // Output: Alice
+deepCopy.address.country = "Canada";
+console.log("deep copy person address country:", person.address.country); // Output: USA :bcoz we created a new object in deep copy so changes in deep copy will not reflect in original object
