6353a0595bb6e468bfb3ec86a6ed9e55d6a52752
Sumeet Borole <sumeet21borole@gmail.com>
2025-11-03 23:56:35 +0530

    assigment

--- javascript question/shallow-deep-copy.js
+++ javascript question/shallow-deep-copy.js
@@ -10,10 +10,10 @@ const person = {
 // Shallow Copy
 const shallowCopy = { ...person };
 shallowCopy.name = "Bob";
-console.log("shallow copy person name:", person.name); // Output: Bob
+console.log("shallow copy person name:", shallowCopy.name); // Output: Bob
 console.log("shallow copy person name:", person.name); // Output: Alice
 shallowCopy.address.city = "Los Angeles";
-console.log("shallow copy person address city:", person.address.city); // Output: Los Angeles  :bcoz { },[] ,() are reference types so when we change in shallow copy it will reflect in original object
+console.log("shallow copy person address city:", person.address.city); // Output: Los Angeles  :bcoz nested objects { },[] ,() are reference types so when we change in shallow copy it will reflect in original object
 
 // Deep Copy
 const deepCopy = JSON.parse(JSON.stringify(person));
@@ -22,3 +22,13 @@ console.log("deep copy person name:", deepCopy.name); // Output: Charlie
 console.log("deep copy person name:", person.name); // Output: Alice
 deepCopy.address.country = "Canada";
 console.log("deep copy person address country:", person.address.country); // Output: USA :bcoz we created a new object in deep copy so changes in deep copy will not reflect in original object
+
+// Assignment - both variables point to the same object
+const value = {
+  name: "John",
+  age: 25,
+};
+
+const copy = value;
+copy.name = "Doe";
+console.log("original value name:", value.name); // Output : Doe : because both value and copy are pointing to same memory location
