0dadc86e42185ad0f5ebb06f81c17a7dbe6b0df8
Sumeet Borole <sumeet21borole@gmail.com>
2025-11-12 12:28:12 +0530

    str

---
+++ javascript question/string.js
@@ -0,0 +1,28 @@
+const str = "Hello, World!";
+console.log(str.toUpperCase()); // Output: "HELLO, WORLD!"
+console.log(str.toLowerCase()); // Output: "hello, world!"
+console.log(str.length); // Output: 13
+console.log(str.charAt(7)); // Output: "W"
+console.log(str.indexOf("World")); // Output: 7
+
+//modify string  - as string are immutable in js , we need to create a new string
+let modifiedStr = str.replace("World", "JavaScript");
+console.log(modifiedStr); // Output: "Hello, JavaScript!"
+
+//searching in string
+console.log(str.includes("Hello")); // Output: true
+console.log(str.indexOf("o")); // Output: 4
+console.log(str.startsWith("Hello")); // Output: true
+console.log(str.endsWith("!")); // Output: true
+
+//trim string
+const strWithSpaces = "   Hello, World!   ";
+console.log(strWithSpaces.trim()); // Output: "Hello, World!"
+console.log(strWithSpaces.trimStart()); // Output: "Hello, World!   "
+console.log(strWithSpaces.trimEnd()); // Output: "   Hello, World!"
+
+//exract substring
+const anotherStr = "JavaScript is awesome";
+console.log(anotherStr.substring(0, 10)); // Output: "JavaScript"
+console.log(anotherStr.substring(13)); // Output: "awesome"
+console.log(anotherStr.slice(13, -1)); // Output: "awesome"
