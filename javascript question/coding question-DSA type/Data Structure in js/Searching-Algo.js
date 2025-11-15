abd46be1989ba3c527eaee46d2cfe1d05f310263
Sumeet Borole <sumeet21borole@gmail.com>
2025-11-15 23:31:54 +0530

    global linear search

--- javascript question/coding question-DSA type/Data Structure in js/Searching-Algo.js
+++ javascript question/coding question-DSA type/Data Structure in js/Searching-Algo.js
@@ -1,7 +1,6 @@
 const arr = [10, 20, 30, 40, 50];
 
 // Linear Search
-
 function linearsearch(array, target) {
   for (let i = 0; i < array.length; i++) {
     if (array[i] === target) {
@@ -12,3 +11,17 @@ function linearsearch(array, target) {
 }
 console.log(linearsearch(arr, 30)); // Output: 2
 //time complexity O(n)   space complexity O(1)
+
+// global linear search : when same element present multiple times in array than we can return all the index of that element
+function globallinearsearch(arr, target) {
+  const result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === target) {
+      result.push(i);
+    }
+  }
+  if (result.length === 0) return -1;
+  return result;
+}
+console.log(globallinearsearch([10, 20, 30, 40, 30, 50], 30)); // Output: [2, 4]
+//time complexity O(n)   space complexity O(n)
