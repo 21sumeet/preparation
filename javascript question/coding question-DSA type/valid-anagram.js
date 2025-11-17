8a936a60cf3b0a1573605e1f0e26f4161d20e54b
Sumeet Borole <sumeet21borole@gmail.com>
2025-11-17 15:07:52 +0530

    optimized approach

--- javascript question/coding question-DSA type/valid-anagram.js
+++ javascript question/coding question-DSA type/valid-anagram.js
@@ -9,3 +9,24 @@ function validanagram(str1, str2) {
 } //time complexity: O(n log n) due to sorting || space complexity: O(1)
 console.log(validanagram("listen", "silent")); // true
 console.log(validanagram("hello", "billion")); // false
+
+//optimized approach using frequency counter object
+function validanagramoptimized(str1, str2) {
+  if (str1.length !== str2.length) return false;
+  let frequencyCounter1 = {};
+  let frequencyCounter2 = {};
+  for (let i = 0; i < str1.length; i++) {
+    let char1 = str1[i];
+    let char2 = str2[i];
+    frequencyCounter1[char1] = (frequencyCounter1[char1] || 0) + 1;
+    frequencyCounter2[char2] = (frequencyCounter2[char2] || 0) + 1;
+  }
+  for (let key in frequencyCounter1) {
+    if (frequencyCounter1[key] !== frequencyCounter2[key]) {
+      return false;
+    }
+  }
+  return true;
+} //time complexity: O(n) || space complexity: O(n)
+console.log(validanagramoptimized("listen", "silent")); // true
+console.log(validanagramoptimized("hello", "billion")); // false
