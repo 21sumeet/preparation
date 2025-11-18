f12cd44b99feb94929ec3e671c08e78292990058
Sumeet Borole <sumeet21borole@gmail.com>
2025-11-18 20:56:25 +0530

    question

---
+++ javascript question/question.js
@@ -0,0 +1,46 @@
+//most frequent char in string
+function freq(str) {
+  const freqobj = {};
+  let maxcount = 0;
+  let ans = [];
+
+  // Count frequencies and find max count
+  for (let char of str) {
+    if (freqobj[char]) {
+      freqobj[char]++;
+    } else {
+      freqobj[char] = 1;
+    }
+    if (freqobj[char] > maxcount) {
+      maxcount = freqobj[char];
+    }
+  }
+  // Find all characters with max count
+  for (let char of str) {
+    if (freqobj[char] == maxcount) {
+      ans.push(char);
+    }
+  }
+  console.log("Frequency object:", freqobj);
+  console.log("Max count:", maxcount);
+  return ans;
+}
+console.log(freq("programing"));
+
+// function nonrepeat(str) {
+//    let charfreq ={};
+//     for(let char of str){
+//         if (charfreq[char]) {
+//             charfreq[char]++;
+//         }else{
+//             charfreq[char]=1;
+//         }
+//     }
+//     console.log(charfreq);
+//     for (let char of str) {
+//         if(charfreq[char]==1){
+//             console.log(char);
+//         }
+//     }
+// }
+// nonrepeat("level")
