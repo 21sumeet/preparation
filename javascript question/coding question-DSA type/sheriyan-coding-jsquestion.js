8660f379f6f4c9511b773f9fe0c131ff62569a74
Sumeet Borole <sumeet21borole@gmail.com>
2025-11-28 19:16:24 +0530

    cmt

--- javascript question/coding question-DSA type/sheriyan-coding-jsquestion.js
+++ javascript question/coding question-DSA type/sheriyan-coding-jsquestion.js
@@ -1,13 +1,16 @@
+//duplicate an array in sucbh a way that original array elements are followed by the same elements
 function duplicate(arr) {
   return [...arr, ...arr];
 }
 console.log(duplicate([1, 2, 4]));
 
+//reverse a number
 function reversenum(num) {
   return Number(num.toString().split("").reverse().join(""));
 }
 console.log(reversenum(123));
 
+//check whether a string is palindrome or not
 function palstring(str) {
   let str1 = str.split("").reverse().join("");
   return str == str1;
