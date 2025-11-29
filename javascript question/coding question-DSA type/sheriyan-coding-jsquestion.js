a78a6167b18276100237e42efbf83e11ff280c31
Sumeet Borole <sumeet21borole@gmail.com>
2025-11-29 12:05:28 +0530

    sheriyan js questions

--- javascript question/coding question-DSA type/sheriyan-coding-jsquestion.js
+++ javascript question/coding question-DSA type/sheriyan-coding-jsquestion.js
@@ -16,3 +16,44 @@ function palstring(str) {
   return str == str1;
 }
 console.log(palstring("loop"));
+
+//aphabetical order arrange
+function alphabeticalorder(str) {
+  return str.split("").sort().join("");
+}
+console.log(alphabeticalorder("helloworld"));
+
+//uppercase of senctence -capitalize
+function capitilize(str) {
+  var sentence = str.split(" ").map((word) => {
+    return word.charAt(0).toUpperCase() + word.substring(1);
+  });
+  return sentence.join(" ");
+}
+console.log(capitilize("elon bhai nacho"));
+
+//freq of char in string
+function frequency(str) {
+  //using object
+  let freq = {};
+  for (var char of str) {
+    if (freq[char]) {
+      freq[char]++;
+    } else {
+      freq[char] = 1;
+    }
+  }
+  console.log(freq);
+
+  //using map
+  var map = new Map();
+  for (var char of str) {
+    if (map.has(char)) {
+      map.set(char, map.get(char) + 1);
+    } else {
+      map.set(char, 1);
+    }
+  }
+  console.log(map);
+}
+console.log(frequency("bumblebee"));
