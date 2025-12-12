23c0ea5f8ed945c4c3394684ee7ebff9cc1950c5
Sumeet Borole <sumeet21borole@gmail.com>
2025-12-12 19:44:09 +0530

    global error handler

--- Elib/src/app.ts
+++ Elib/src/app.ts
@@ -1,8 +1,21 @@
-import express from"express";
+import express, { type NextFunction, type Request, type Response } from"express";
+import type { HttpError } from "http-errors";
+import { config } from "../config/config.ts";
 const app = express();
 
 app.get("/", (req , res)=>{
     res.json({message:"hello from ebook api backend"})
+})
+
+
+//global error handler
+app.use((err :HttpError , req :Request ,res:Response , next:NextFunction)=>{
+    const statuscode = err.statusCode || 500;
+    res.status(statuscode).json({
+        message : err.message,
+        errorStack : config.env==="development" ? err.stack:"",
+    })
+
 })
 
 export default app ;
