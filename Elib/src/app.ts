c54c601b2c053830ca4e28a7a67d4369e719423d
Sumeet Borole <sumeet21borole@gmail.com>
2025-12-17 20:13:21 +0530

    rate limiting for Elib backend

--- Elib/src/app.ts
+++ Elib/src/app.ts
@@ -1,10 +1,21 @@
 import express, { type NextFunction, type Request, type Response } from"express";
 import type { HttpError } from "http-errors";
 import { config } from "../config/config.ts";
+import { rateLimit } from "express-rate-limit";
+import Userrouter from "../user/userRoute.ts";
 
 const app = express();
 app.use(express.json());
-import Userrouter from "../user/userRoute.ts";
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+  message: "Too many requests from this IP, please try again after 15 minutes",
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+app.use(limiter);
+
 
 app.get("/", (req , res)=>{
     res.json({message:"hello from ebook api backend"})
