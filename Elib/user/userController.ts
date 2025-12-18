2db20eed1115b903f4ce3655a2c1eca83a224829
Sumeet Borole <sumeet21borole@gmail.com>
2025-12-18 20:07:25 +0530

    creating newuser

--- Elib/user/userController.ts
+++ Elib/user/userController.ts
@@ -1,12 +1,13 @@
 import express, { type NextFunction, type Request, type Response } from"express";
 import createHttpError from "http-errors";
 import userModel from "./userModel.ts";
+import bcrypt from "bcrypt"
 
 
 const createuser = async(req:Request , res:Response , next:NextFunction)=>{
     //validation 
-    const {name , email , password} = req.body;
-    if(!name || !email || !password){
+    const {name , email , password , age} = req.body;
+    if(!name || !email || !password ||!age){
         const error = createHttpError(400 , "All field is required ");
         return next(error);
     }
@@ -15,6 +16,19 @@ const createuser = async(req:Request , res:Response , next:NextFunction)=>{
         const error = createHttpError(400 , "user already exist ");
         return next(error);
     }
+    //creating newuser
+    const hashpassword =  await bcrypt.hash(password , 10);
+    const newuser = await userModel.create({
+        name ,
+        email ,
+        password : hashpassword,
+        age
+    })
+    res.json({
+        id:newuser._id,
+        message :  `${newuser.name} is created`
+    })
+
 }
 
 export {createuser};
