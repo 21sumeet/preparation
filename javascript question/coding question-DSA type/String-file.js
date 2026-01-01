964d739495ed388b64aa1695ebfbe563e1a47e7c
Sumeet Borole <sumeet21borole@gmail.com>
2026-01-01 20:47:32 +0530

    String file dsa questions

--- javascript question/coding question-DSA type/String-file.js
+++ javascript question/coding question-DSA type/String-file.js
@@ -89,3 +89,116 @@ console.log(nonrepeating("abcasc"));
 //     }
 //     return null;
 // }
+
+//Anagram Check- JavaScript
+function areAnagrams(str1, str2) {
+  if (str1.length !== str2.length) return false;
+
+  const sorted1 = str1.split("").sort().join("");
+  const sorted2 = str2.split("").sort().join("");
+  return sorted1 === sorted2;
+}
+// // Java
+// public static boolean areAnagrams(String str1, String str2) {
+//     if(str1.length() != str2.length()) return false;
+
+//     char[] arr1 = str1.toCharArray();
+//     char[] arr2 = str2.toCharArray();
+//     Arrays.sort(arr1);
+//     Arrays.sort(arr2);
+//     return Arrays.equals(arr1, arr2);
+// }
+
+// Remove Duplicates - javascript
+function removeduplicate(str) {
+  let seen = new Map();
+  let result = "";
+  for (let char of str) {
+    if (!seen.has(char)) {
+      seen.set(char);
+      result += char;
+    }
+  }
+  return result;
+}
+console.log(removeduplicate("abcabdef"));
+// //java
+// public static String removeDuplicates(String str) {
+//     Set<Character> seen = new HashSet<>();
+//     for(char ch : str.toCharArray()) {
+//         seen.add(ch);
+//     }
+
+//     StringBuilder result = new StringBuilder();
+//     for(char ch : seen) {
+//         result.append(ch);
+//     }
+//     return result.toString();
+// }
+
+// Most Frequent Character- JavaScript
+function mostFrequentChar(str) {
+  const freq = new Map();
+  let maxChar = "";
+  let maxCount = 0;
+
+  for (let char of str) {
+    const count = (freq.get(char) || 0) + 1;
+    freq.set(char, count);
+
+    if (count > maxCount) {
+      maxCount = count;
+      maxChar = char;
+    }
+  }
+  return maxChar;
+}
+// // Java
+// public static char mostFrequentChar(String str) {
+//     Map<Character, Integer> freq = new HashMap<>();
+//     char maxChar = ' ';
+//     int maxCount = 0;
+
+//     for(char ch : str.toCharArray()) {
+//         int count = freq.getOrDefault(ch, 0) + 1;
+//         freq.put(ch, count);
+
+//         if(count > maxCount) {
+//             maxCount = count;
+//             maxChar = ch;
+//         }
+//     }
+//     return maxChar;
+// }
+
+// convert single char uppercase tolowercase and lowercase to uppercase  : Hello World -> hELLO wORLD
+function convertcase(str) {
+  let result = "";
+  for (let char of str) {
+    if (char === char.toUpperCase()) {
+      result += char.toLowerCase();
+    } else {
+      result += char.toUpperCase();
+    }
+  }
+  return result;
+}
+console.log(convertcase("JavaScript"));
+// //java
+// public static String toggleCase(String str) {
+//         StringBuilder result = new StringBuilder();
+
+//         for(int i = 0; i < str.length(); i++) {
+//             char ch = str.charAt(i);
+
+//             if(Character.isUpperCase(ch)) {
+//                 result.append(Character.toLowerCase(ch));
+//             } else if(Character.isLowerCase(ch)) {
+//                 result.append(Character.toUpperCase(ch));
+//             } else {
+//                 result.append(ch);  // Keep numbers, symbols unchanged
+//             }
+//         }
+
+//         return result.toString();
+// }
