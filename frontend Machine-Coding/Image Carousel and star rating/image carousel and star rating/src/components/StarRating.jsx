0cc211b87d70e00c72193b7d5ccccfa3e0df901b
Sumeet Borole <sumeet21borole@gmail.com>
2026-01-03 19:35:59 +0530

    hover effect in star rating

--- frontend Machine-Coding/Image Carousel and star rating/image carousel and star rating/src/components/StarRating.jsx
+++ frontend Machine-Coding/Image Carousel and star rating/image carousel and star rating/src/components/StarRating.jsx
@@ -3,17 +3,20 @@ import "./starrating.css";
 
 const StarRating = ({ STAR_COUNT = 5 }) => {
   const [active, setActive] = useState();
+  const [hovervalue, sethovervalue] = useState();
 
   return (
     <div className="container">
       {new Array(STAR_COUNT).fill(0).map((_, index) => (
         <span
           key={index}
-          className={index <= active ? "active" : ""}
+          className={index <= active || index <= hovervalue ? "active" : ""}
           onClick={() => {
             console.log("Clicked star:", index + 1);
             setActive(index);
           }}
+          onMouseEnter={() => sethovervalue(index + 1)}
+          onMouseLeave={() => sethovervalue(0)}
         >
           &#9733;
         </span>
