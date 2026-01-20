b0ca2905be387014c51fd1a459c464d7d68b8c3f
Sumeet Borole <sumeet21borole@gmail.com>
2026-01-20 15:01:52 +0530

    funciton hoisitng

---
+++ javascript question/function-hoisting.js
@@ -0,0 +1,13 @@
+name(); //Sumit
+var name = function () {
+  console.log("Sumeet");
+};
+name(); //sumeet
+
+function name() {
+  console.log("Sumit");
+}
+name(); //sumeet
+
+//How ? // Function declaration goes to top COMPLETELY
+// Variable declaration hoists, but not the assignment
